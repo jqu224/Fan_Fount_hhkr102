44ba691e41dfafb601b1bc57db433203e3fa12fa
Jcq <q.roggee@gmail.com>
2019-04-03 19:24:37 -0700

    Update Day1_prob21_40.sql

--- 1_sql_hkkr/Day1_prob21_40.sql
+++ 1_sql_hkkr/Day1_prob21_40.sql
@@ -55,11 +55,12 @@ group by RowNumber;
 25
 ------------------------------------------------------------------------------
 -- https://www.hackerrank.com/challenges/binary-search-tree-1/problem
-SELECT CASE
+SELECT 
+CASE
 	WHEN P IS NULL THEN CONCAT(N, ' Root')
 	WHEN N IN (SELECT DISTINCT P FROM BST) THEN CONCAT(N, ' Inner')
 	ELSE CONCAT(N, ' Leaf')
-	END
+END
 FROM BST
 ORDER BY N ASC
 
