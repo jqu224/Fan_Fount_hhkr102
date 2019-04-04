6610b75ce5444724c1eec7374450f268067c858c
Jcq <q.roggee@gmail.com>
2019-04-03 22:02:01 -0700

    Update Day_0_prob20.sql

--- 3_sql_leetcode/Day_0_prob20.sql
+++ 3_sql_leetcode/Day_0_prob20.sql
@@ -11,27 +11,44 @@ FROM World
 WHERE area > 3000000 or population > 25000000
         
 
-1
+2
+-----------------------------------------
+SELECT MIN(ABS(p1.x - p2.x)) AS `shortest`
+FROM point p1, point p2
+WHERE p1.x != p2.x
+;
+# 
+# SELECT MIN(ABS(P1.x - P2.x)) AS shortest 
+# FROM point AS P1
+# JOIN point AS P2 ON P1.x <> P2.x
+
+3
+-----------------------------------------
+UPDATE salary
+set sex =
+CASE sex
+    when 'm' then 'f'
+    WHEN 'f' then 'm'
+end
+
+4
+-----------------------------------------
+https://leetcode.com/problems/find-customer-referee
+ # Write your MySQL query statement below
+SELECT name
+FROM customer
+WHERE referee_id != 2 or referee_id is null
+               
+
+5
 -----------------------------------------
 
 
-1
+6
 -----------------------------------------
 
 
-1
------------------------------------------
-
-
-1
------------------------------------------
-
-
-1
------------------------------------------
-
-
-1
+7
 -----------------------------------------
 
 
