e55e7bb93b7246977fffad1e7ebb1e969c77955f
Jcq <q.roggee@gmail.com>
2019-04-04 18:59:20 -0700

    Update 8_employee_bonus.sql

--- 3_sql_leetcode/8_employee_bonus.sql
+++ 3_sql_leetcode/8_employee_bonus.sql
@@ -35,3 +35,11 @@ Example ouput:
 
 
 🙊--------ANS---------ANS---------ANS---------ANS---------ANS---------ANS---------ANS
+# Write your MySQL query statement below
+SELECT
+    E.name
+    , B.bonus
+FROM Employee AS E
+LEFT JOIN Bonus AS B
+ON E.empId = B.empId 
+WHERE B.bonus < 1000 or B.bonus is null
