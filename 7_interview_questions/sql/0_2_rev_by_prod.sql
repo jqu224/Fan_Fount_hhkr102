9e5b1fd9d6d5ac7555550d7d4b3d5a27860c908e
Jcq <q.roggee@gmail.com>
2019-08-04 17:32:58 -0700

    Create 0_2_rev_by_prod.sql

---
+++ 7_interview_questions/sql/0_2_rev_by_prod.sql
@@ -0,0 +1,9 @@
+https://www.1point3acres.com/bbs/thread-540456-1-1.html
+```
+SQL题
+1. write a query to return the top10 customer          
+2. customer rev by product in 2019 Sum &left join
+3. # of customer by month by product 同上，稍微有点记不清了
+```
+
+    
