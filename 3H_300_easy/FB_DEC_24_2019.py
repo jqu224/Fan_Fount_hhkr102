76669ef06a4ec6202c403fb867ab737735a2c247
Gkrjquibbler <q.roggee@gmail.com>
2019-12-24 22:07:39 -0800

    Update FB_DEC_24_2019.py

--- 3H_300_easy/FB_DEC_24_2019.py
+++ 3H_300_easy/FB_DEC_24_2019.py
@@ -1,50 +1,41 @@
 """
-523. Continuous Subarray Sum
-https://leetcode.com/problems/continuous-subarray-sum/
+426. Convert Binary Search Tree to Sorted Doubly Linked List
+https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/
 """
-class Solution:
-    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        if len(nums) < 2:
-            return False
-        if not k:
-            prev = nums[0]
-            c = 0
-            for i in nums[1:]:
-                if i == 0 and prev == 0:
-                    return True
-                elif i == 0:
-                    c, prev = 1, 0
-                else:
-                    c, prev = 0, i
-            return False
-        sum_ = 0
-        dict_ = {}
-        for i, n in enumerate(nums):
-            sum_ += n
-            rem = sum_ % k 
-            if rem != 0:
-                if rem not in dict_:
-                    dict_[rem] = i, sum_
-                else:
-                    if i - dict_[rem][0] > 1: 
-                        return True
-            elif i > 0:
-                return True
-        return False
-    """
-    pre_sum
-    """
-    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        mp = {0: -1}
-        prefix_sum = 0
-        for i, num in enumerate(nums):
-            prefix_sum += num
-            if k != 0:
-                prefix_sum = prefix_sum % k
-            if prefix_sum in mp:
-                if i - mp[prefix_sum] > 1:
-                    return True
-            else:
-                mp[prefix_sum] = i
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
 
-        return False
+class Solution:
+    first, last = None, None
+    def treeToDoublyList(self, root: 'Node') -> 'Node': 
+        if not root:
+            return None 
+        def helper(node): 
+            if node:
+                # left
+                helper(node.left)
+                # node 
+                if self.last:
+                    # link the previous node (last)
+                    # with the current one (node)
+                    self.last.right = node
+                    node.left = self.last
+                else:
+                    # keep the smallest node
+                    # to close DLL later on
+                    self.first = node        
+                self.last = node
+                # right
+                helper(node.right)
+        
+        helper(root)
+        # close DLL
+        self.last.right = self.first
+        self.first.left = self.last
+        return self.first
