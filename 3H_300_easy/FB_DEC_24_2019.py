8ea0abaf9a87df7624dee25d95300de810aa8153
Gkrjquibbler <q.roggee@gmail.com>
2019-12-24 21:46:18 -0800

    Update FB_DEC_24_2019.py

--- 3H_300_easy/FB_DEC_24_2019.py
+++ 3H_300_easy/FB_DEC_24_2019.py
@@ -2,3 +2,32 @@
 523. Continuous Subarray Sum
 https://leetcode.com/problems/continuous-subarray-sum/
 """
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        if len(nums) < 2:
+            return False
+        if not k:
+            prev = nums[0]
+            c = 0
+            for i in nums[1:]:
+                if i == 0 and prev == 0:
+                    return True
+                elif i == 0:
+                    c, prev = 1, 0
+                else:
+                    c, prev = 0, i
+            return False
+        sum_ = 0
+        dict_ = {}
+        for i, n in enumerate(nums):
+            sum_ += n
+            rem = sum_ % k 
+            if rem != 0:
+                if rem not in dict_:
+                    dict_[rem] = i, sum_
+                else:
+                    if i - dict_[rem][0] > 1: 
+                        return True
+            elif i > 0:
+                return True
+        return False
