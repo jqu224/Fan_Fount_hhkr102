734efd33fd0d23e4cbb6a3082ba7bad24fa88603
Gkrjquibbler <q.roggee@gmail.com>
2019-12-27 23:48:40 -0800

    Update FB_DEC_27_2019.py

--- 3H_300_easy/FB_DEC_27_2019.py
+++ 3H_300_easy/FB_DEC_27_2019.py
@@ -72,3 +72,36 @@ class Solution:
                         # set dp to True where next word search starts
                         dp[i + len(w)] = True
         return dp[-1]
+
+146. LRU Cache
+from collections import OrderedDict
+class LRUCache(OrderedDict):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self:
+            return - 1
+        
+        self.move_to_end(key)
+        return self[key]
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key in self:
+            self. (key)
+        self[key] = value
+        if len(self) > self.capacity:
+            self.popitem(last = False)
