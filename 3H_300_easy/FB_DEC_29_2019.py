fd60f6d195c15b2e18c94975bc850a9e0310c68b
Gkrjquibbler <q.roggee@gmail.com>
2019-12-29 21:01:24 -0800

    Update FB_DEC_29_2019.py

--- 3H_300_easy/FB_DEC_29_2019.py
+++ 3H_300_easy/FB_DEC_29_2019.py
@@ -1,1 +1,24 @@
+1300. Sum of Mutated Array Closest to Target
 
+class Solution:    
+    def findBestValue(self, A, target):
+        A.sort(reverse=1)
+        maxA = A[0]
+        while A and target >= A[-1] * len(A):
+            target -= A.pop()
+        if A:
+            return int((target + len(A) / 2.0 - 0.1) / len(A))
+        else:
+            return maxA
+          
+class 
+sort arr
+get t//l
+if t//l <= arr[0] return t//l
+else 
+  for i, n in enumerate(arr) 
+    if (t - n) // (l-i) > next
+      pass
+    else:
+      return (t - n) // (l-i)
+      
