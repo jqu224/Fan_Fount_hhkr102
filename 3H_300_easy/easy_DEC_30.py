890fc61ea779ca75fb49a5c9e824027559ec9721
Gkrjquibbler <q.roggee@gmail.com>
2019-12-30 22:10:40 -0800

    Update easy_DEC_30.py

--- 3H_300_easy/easy_DEC_30.py
+++ 3H_300_easy/easy_DEC_30.py
@@ -14,3 +14,37 @@ class Solution:
             return 0
         else:
             return answer
+        
+509. Fibonacci Number
+class Solution:
+    def fib(self, N: int) -> int:
+        def dfs(n):
+            if n > 1: 
+                return dfs(n-1) + dfs(n-2)
+            else:
+                return n
+        return dfs(N)
+    
+class Solution:
+    def fib(self, N: int) -> int:
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+        
+        
+        return self.fib(N-1) + self.fib(N-2)
+    
+class Solution:
+    def fib(self, N: int) -> int:
+        if N < 2:
+            return N
+        
+        a, b = 0, 1
+        result = 0
+        for i in range(2, N+1):
+            result = a + b
+            a = b
+            b = result
+             
+        return result
