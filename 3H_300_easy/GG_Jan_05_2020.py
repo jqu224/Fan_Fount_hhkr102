b066c028f07b5fd9f87445564c792dd26b4a9b86
Gkrjquibbler <q.roggee@gmail.com>
2020-01-05 20:36:23 -0800

    Create GG_Jan_05_2020.py

---
+++ 3H_300_easy/GG_Jan_05_2020.py
@@ -0,0 +1,35 @@
+
+430. Flatten a Multilevel Doubly Linked List
+https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+class Solution:
+    
+    def flatten(self, head: 'Node') -> 'Node':
+        if not head:
+            return None
+        
+        stack, prev = [head], None
+        
+        while stack:
+            curr = stack.pop()
+            
+            if prev:
+                prev.next, curr.prev = curr, prev
+            
+            prev = curr
+            
+            if curr.next:
+                stack.append(curr.next)
+            
+            if curr.child:
+                stack.append(curr.child)
+                curr.child = None
+        return head
