14ba9039ef90fc40f6e50ebb7a52cb2e830751aa
Gkrjquibbler <q.roggee@gmail.com>
2020-01-07 21:27:22 -0800

    Update and rename GG_JAN_07_2020.py to GG_Jan_07_2020.py

--- 3H_300_easy/GG_Jan_07_2020.py
+++ 3H_300_easy/GG_Jan_07_2020.py
@@ -25,3 +25,31 @@ class Solution:
                             stack.append(coor)
                 num_left += 1
         return len(stones) - num_left
+
+class Solution(object):
+    def removeStones(self, stones):
+        graph = collections.defaultdict(list)
+        for i, x in enumerate(stones):
+            for j in range(i):
+                y = stones[j]
+                if x[0]==y[0] or x[1]==y[1]:
+                    graph[i].append(j)
+                    graph[j].append(i)
+
+        N = len(stones)
+        ans = 0
+
+        seen = [False] * N
+        for i in range(N):
+            if not seen[i]:
+                stack = [i]
+                seen[i] = True
+                while stack:
+                    ans += 1
+                    node = stack.pop()
+                    for nei in graph[node]:
+                        if not seen[nei]:
+                            stack.append(nei)
+                            seen[nei] = True
+                ans -= 1
+        return ans
