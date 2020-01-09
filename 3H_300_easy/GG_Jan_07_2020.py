15ea54965f791a6b950b1ee9f3438e12d3adac71
Gkrjquibbler <q.roggee@gmail.com>
2020-01-08 20:34:33 -0800

    Update GG_Jan_07_2020.py

--- 3H_300_easy/GG_Jan_07_2020.py
+++ 3H_300_easy/GG_Jan_07_2020.py
@@ -55,3 +55,64 @@ class Solution(object):
                             seen[nei] = True
                 ans -= 1
         return ans
+    
+939. Minimum Area Rectangle
+class Solution:
+    def minAreaRect(self, points: List[List[int]]) -> int:
+        # bfs
+        min_ = float("inf")
+        seen = set()
+        l_dict = collections.defaultdict(list)
+        r_dict = collections.defaultdict(list)
+        for l, r in points:
+            l_dict[l].append(r)
+            r_dict[r].append(l) 
+            
+        l_line = collections.defaultdict(list)
+        
+        for l, list_r in l_dict.items():
+            list_r = sorted(list_r)
+            for i, r in enumerate(list_r):
+                for j in range(i):
+                    l_line[(list_r[j], r)].append(l) 
+                    
+        for (low, hi), v in l_line.items():
+            hight = hi - low
+            if hight < min_:
+                list_x = sorted(v)
+                for i in range(len(list_x)-1):
+                    wid = list_x[i+1] - list_x[i]
+                    if wid < min_:
+                        min_ = min(min_, wid*hight)
+                        
+        return min_ if min_ != float("inf") else 0
+            import collections
+        
+class Solution:
+    def minAreaRect(self, points):
+        n = len(points)
+        nx = len(set(x for x, y in points))
+        ny = len(set(y for x, y in points))
+        if nx == n or ny == n:
+            return 0
+
+        p = collections.defaultdict(list)
+        if nx > ny:
+            for x, y in points:
+                p[x].append(y)
+        else:
+            for x, y in points:
+                p[y].append(x)
+ 
+        lastx = {}
+        res = float('inf')
+        for x in sorted(p):
+            p[x].sort()
+            for i in range(len(p[x])):
+                for j in range(i):
+                    y1, y2 = p[x][j], p[x][i]
+                    if (y1, y2) in lastx:
+                        res = min(res, (x - lastx[y1, y2]) * (y2 - y1))
+                    lastx[y1, y2] = x
+        return res if res < float('inf') else 0
+            
