d60f2bfbbb0ba5fa30a95d8343ac0fc7ec3c1dcd
Gkrjquibbler <q.roggee@gmail.com>
2020-01-10 22:56:01 -0800

    Update and rename GG_JAN_10_2020.py to GG_Jan_10_2020.py

---
+++ 3H_300_easy/GG_Jan_10_2020.py
@@ -0,0 +1,15 @@
+904. Fruit Into Baskets
+https://leetcode.com/problems/fruit-into-baskets/
+class Solution(object):
+    def totalFruit(self, tree):
+        ans = i = 0
+        count = collections.Counter()
+        for j, x in enumerate(tree):
+            count[x] += 1
+            while len(count) >= 3:
+                count[tree[i]] -= 1
+                if count[tree[i]] == 0:
+                    count.pop(tree[i])
+                i += 1
+            ans = max(ans, j - i + 1)
+        return ans
