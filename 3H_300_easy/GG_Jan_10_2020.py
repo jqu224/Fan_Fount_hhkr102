4e9636cb7fb00f5a22d1ba8cd4f605feba091ddb
Gkrjquibbler <q.roggee@gmail.com>
2020-01-10 22:58:07 -0800

    Update GG_Jan_10_2020.py

--- 3H_300_easy/GG_Jan_10_2020.py
+++ 3H_300_easy/GG_Jan_10_2020.py
@@ -13,3 +13,14 @@ class Solution(object):
                 i += 1
             ans = max(ans, j - i + 1)
         return ans
+    
+class Solution(object):
+    def totalFruit(self, tree):
+        count, i = {}, 0
+        for j, v in enumerate(tree):
+            count[v] = count.get(v, 0) + 1
+            if len(count) > 2:
+                count[tree[i]] -= 1
+                if count[tree[i]] == 0: del count[tree[i]]
+                i += 1
+        return j - i + 1
