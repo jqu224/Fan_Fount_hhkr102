7693b817b6911232a7f4e19e948e5f6b8cfc7910
Gkrjquibbler <q.roggee@gmail.com>
2020-01-14 21:51:46 -0800

    Update GG_Jan_14_2020.py

--- 3H_300_easy/GG_Jan_14_2020.py
+++ 3H_300_easy/GG_Jan_14_2020.py
@@ -15,3 +15,25 @@ class Solution:
                     continue
                 return False 
         return True
+    
+47. Permutations II
+https://leetcode.com/problems/permutations-ii/
+class Solution: 
+    def permuteUnique(self, nums): # dfs
+
+        nums.sort() 
+        perms = []
+
+        def _permutation(perm, left):
+            # perm: 
+            # left: what is left to add to perm 
+            if len(perm) == len(nums):
+                perms.append(perm)
+            else:
+                for i in range(len(left)):
+                    if i != 0 and left[i] == left[i-1]: # avoid duplicate
+                        continue
+                    _permutation(perm+[left[i]], left[:i]+left[(i+1):])
+
+        _permutation([], nums)
+        return perms
