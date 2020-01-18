042da750ec4b4d62d6ce34f43f9ecbee2f683fbe
Gkrjquibbler <q.roggee@gmail.com>
2020-01-17 21:06:00 -0800

    Update GG_Jan_17_2020.py

--- 3H_300_easy/GG_Jan_17_2020.py
+++ 3H_300_easy/GG_Jan_17_2020.py
@@ -16,3 +16,15 @@ class Solution:
             stack = temp
         return -1
             
+class Solution:
+    def numSquares(self, n: int) -> int:
+        squares = [i**2 for i in range(1, int(n**0.5)+1)]
+        d, q, nq = 1, {n}, set()
+        while q:
+            for node in q:
+                for square in squares:
+                    if node == square: return d
+                    if node < square: break
+                    nq.add(node-square)
+            q, nq, d = nq, set(), d+1
+
