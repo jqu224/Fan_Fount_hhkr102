6f2efecade86b2eb4a9b9a1a647531ee49ffec27
Gkrjquibbler <q.roggee@gmail.com>
2020-01-17 21:33:05 -0800

    Update GG_Jan_16_2020.py

--- 3H_300_easy/GG_Jan_16_2020.py
+++ 3H_300_easy/GG_Jan_16_2020.py
@@ -96,3 +96,26 @@ class PeekingIterator:
             return self.iterator.hasNext()
         else:
             return True
+        
+524
+from typing import List
+from collections import OrderedDict
+
+
+class Solution:
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+        d.sort(key=lambda x: (-len(x), x), reverse=True)
+        for cand in d[::-1]:
+            ind = 0
+            for i in cand:
+                found_index = s[ind:].find(i)
+                ind += found_index
+                if found_index != -1:
+                    ind += 1
+                    continue
+                else:
+                    ind = len(s) + 2
+                    break
+            if ind <= len(s):
+                return cand
+        return ""
