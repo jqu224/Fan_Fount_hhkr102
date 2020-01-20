7341abca67e41dafebeda2a1027ccd44d5c0edb2
Gkrjquibbler <q.roggee@gmail.com>
2020-01-19 22:18:11 -0800

    Update LYFT_Jan_19_2020.py

--- 3H_300_easy/LYFT_Jan_19_2020.py
+++ 3H_300_easy/LYFT_Jan_19_2020.py
@@ -32,3 +32,41 @@ class Solution:
         for i in range(len(nums)):
             ret.append(left_r[i]*right_l[i+1])
         return ret    
+    
+365 water and jug
+https://leetcode.com/problems/water-and-jug-problem
+    
+class Solution:
+    def canMeasureWater(self, x: int, y: int, z: int) -> bool:
+        if z == 0:
+            return True
+        if x+y<z:
+            return  False
+        return z % self.gcd(x, y) == 0;
+
+    def gcd(self,a,b):
+        if a%b == 0:
+            return b
+        else :
+            return self.gcd(b,a%b)
+        
+        class Solution:
+    def canMeasureWater(self, x: int, y: int, z: int) -> bool:
+        x, y = (y, x) if y < x else (x, y)
+        if x == z or y == z:
+            return True
+        if x == 0 or y == 0:
+            return False 
+        if z > x + y:
+            return False
+        if z % x == 0:
+            return True
+        if z == x + y:
+            return True 
+        if y % x == 0:
+            return False
+        if math.gcd(x,y) != 1 and z % math.gcd(x,y) != 0:
+            return False
+        if z % math.gcd(x,y) == 0:
+            return True
+        return True
