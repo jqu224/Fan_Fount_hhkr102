35dd450f98a3315c51d25e109e37e09c712708cd
Gkrjquibbler <q.roggee@gmail.com>
2020-01-20 22:59:31 -0800

    Update LYFT_Jan_20_2020.py

--- 3H_300_easy/LYFT_Jan_20_2020.py
+++ 3H_300_easy/LYFT_Jan_20_2020.py
@@ -75,7 +75,7 @@ https://leetcode.com/problems/find-duplicate-subtrees/
 class Solution:
     def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
         self.res = []
-        self.preorder(root, {})
+        self.inorder(root, {})
         return self.res
         
         
@@ -83,9 +83,9 @@ class Solution:
         if not root:
             return ''
         Str = "("
-        Str += self.preorder(root.left, d)  
+        Str += self.inorder(root.left, d)  
         Str += str(root.val)  
-        Str += self.preorder(root.right, d)  
+        Str += self.inorder(root.right, d)  
         Str += ")"
         
         if (Str in d and d[Str] == 1):
@@ -96,3 +96,50 @@ class Solution:
             d[Str] = 1
         
         return Str
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        count = collections.Counter()
+        ans = []
+        def collect(node):
+            if not node: return "#"
+            serial = "{},{},{}".format(collect(node.left), collect(node.right), node.val) 
+            count[serial] += 1
+            if count[serial] == 2:
+                ans.append(node)
+            return serial
+
+        collect(root)
+        return ans   
+    
+406. Queue Reconstruction by Height
+https://leetcode.com/problems/queue-reconstruction-by-height/
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        rank = {}
+        hp = []
+        seen = set()
+        count = collections.Counter([i for i, p in people]) 
+        dt = collections.defaultdict(list)
+        for h, k in people: 
+            dt[h].append(k)
+            if h not in seen:
+                seen.add(h)
+                heapq.heappush(hp, h)
+        cnt = 0
+        while hp:
+            rank[cnt] = heapq.heappop(hp)
+            cnt += 1
+        ret = ["" for i in range(len(people))]
+        memo = [i for i in range(len(people))]
+        for r in sorted(rank): 
+            for inx in sorted(dt[rank[r]])[::-1]: 
+                ret[memo.pop(inx)] = [rank[r], inx] 
+        return ret
+        
